cf727c9b672e928ad8b5ffe302b7fa83a585f54a
shaswat4 <shaswatagrawal01@gmail.com>
2023-02-08 22:20:26 +0530

    added flash animation

--- game.js
+++ game.js
@@ -4,10 +4,19 @@ var gamePattern  = [];
 function nextSequence() {
     var x = Math.random()*4;
     x = Math.floor(x);
-    return x;     
+
+    gamePattern.push( buttonColours[x] );
+
+    buttonFlashAnimation( buttonColours[x] );
+
 }
 
 var randNum = nextSequence();
 var randColor = buttonColours[randNum];
 
 console.log(randColor);
+
+function buttonFlashAnimation( color ){
+    $("."+color).fadeOut(100).fadeIn();
+}
+
