d7a30b49121a06820c64bffaaff11e826629f7e8
shaswat4 <shaswatagrawal01@gmail.com>
2023-02-08 19:56:18 +0530

    Added random number generation and color selection

--- game.js
+++ game.js
@@ -1,1 +1,13 @@
-alert("hello");
+var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern  = [];
+
+function nextSequence() {
+    var x = Math.random()*4;
+    x = Math.floor(x);
+    return x;     
+}
+
+var randNum = nextSequence();
+var randColor = buttonColours[randNum];
+
+console.log(randColor);
