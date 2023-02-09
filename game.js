bed32efb13536b660df53a59fe183ef20bd7b925
shaswat4 <shaswatagrawal01@gmail.com>
2023-02-09 19:16:50 +0530

    added user click handler and its animations and sound

--- game.js
+++ game.js
@@ -1,5 +1,21 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern  = [];
+var gameLevel = 0;
+var userClickedPattern = [];
+
+$(document).keypress( function (){
+    if ( gameLevel == 0){
+        nextSequence();
+    }
+});
+
+$(".btn").click(function (){
+    var userChosenColor = this.id ;
+    playAudio(userChosenColor);
+    animatePress(userChosenColor);
+    console.log(userChosenColor);
+    userClickedPattern.push(userChosenColor);
+});
 
 function nextSequence() {
     var x = Math.random()*4;
@@ -9,22 +25,30 @@ function nextSequence() {
     gamePattern.push( topColor );
 
     buttonFlashAnimation( topColor );
-
     playAudio( topColor);
 
 }
 
-var randNum = nextSequence();
-var randColor = buttonColours[randNum];
 
-console.log(randColor);
+// console.log(randColor);
 
 function buttonFlashAnimation( color ){
     $("."+color).fadeOut(100).fadeIn();
+}
+
+function animatePress( color) {
+    $("."+ color).toggleClass("pressed");
+    setTimeout( function () {
+        $("."+ color).toggleClass("pressed");
+    } ,100);
 }
 
 function playAudio( color ) {
 var audio = new Audio("sounds/" + color +".mp3");
 audio.play();
     
+}
+
+function changeLevel(){
+
 }
